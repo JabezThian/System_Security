152c4fd80947ee8f0232b93bb9080a4d1784ff0c
FeioyFTW <76903927+FeioyFTW@users.noreply.github.com>
2021-06-29 12:21:41 +0800

    Update Nanyang_Polyclinic.sql
    Add Specialization and URL (Melvin)

--- Nanyang_Polyclinic.sql
+++ Nanyang_Polyclinic.sql
@@ -1,22 +1,24 @@
-DROP DATABASE IF EXISTS nanyang_login;
 CREATE DATABASE IF NOT EXISTS nanyang_login;
+DROP TABLE IF EXISTS Users;
 USE nanyang_login;
-DROP TABLE IF EXISTS Accounts;
-CREATE TABLE IF NOT EXISTS Accounts (
+CREATE TABLE IF NOT EXISTS Users (
  nric varchar(9) NOT NULL,
  fname varchar(50) NOT NULL,
  lname varchar(50) NOT NULL,
  gender varchar(1) NOT NULL,
- date_of_birth date NOT NULL,
- email varchar(255) NOT NULL,
+ dob date NOT NULL,
+ email varchar(255),
  password varchar(255) NOT NULL,
  symmetrickey varchar(255) NULL,
  role varchar(50) NOT NULL,
+ specialization varchar(50) NULL,
+ url varchar(255) NULL,
  PRIMARY KEY (nric)
 );
-INSERT INTO `nanyang_login`.`accounts`
-(`nric`, `fname`, `lname`, `gender`, `date_of_birth`, `email`, `password`, `role`)
+INSERT INTO `nanyang_login`.`users`
+(`nric`, `fname`, `lname`, `gender`, `dob`, `email`, `password`, `role`, `specialization`, `url`)
 VALUES
-('T0392511G', 'Daniel', 'Jack', 'M', '1999-10-22', 'danieljack@gmail.com','password', 'Patient'),
-('T1111111F', 'Eric', 'Lee', 'M', '1994-10-30', 'samwilson@gmail.com', 'password', 'Doctor'),
-('T5739128U', 'Chloe', 'Soh', 'F', '1980-10-15', 'chloesoh@gmail.com', 'password', 'Admin');
+('T0392511G', 'Daniel', 'Jack', 'M', '1999-10-22', 'danieljack@gmail.com','password', 'Patient', NULL, NULL),
+('T1111111F', 'Eric', 'Lee', 'M', '1994-10-30', 'samwilson@gmail.com', 'password', 'Doctor', 'Cardiology', 'https://google.com'),
+('T5739128U', 'Chloe', 'Soh', 'F', '1980-10-15', 'chloesoh@gmail.com', 'password', 'Admin', NULL, NULL);
+
