7133294332505553ba7b472d5d0cc19c61c7dce0
JabezThian <56944525+JabezThian@users.noreply.github.com>
2021-08-13 00:25:58 +0800

    Updated SQL by Jabez

--- Nanyang_Polyclinic.sql
+++ Nanyang_Polyclinic.sql
@@ -13,12 +13,14 @@ CREATE TABLE IF NOT EXISTS users (
  role varchar(50) NOT NULL,
  specialization varchar(50) NULL,
  url varchar(255) NULL,
- attempt int,
+ attempt int NOT NULL,
+ lockout varchar(20) NOT NULL,
+ lockout_time varchar(50) NULL,
  PRIMARY KEY (nric)
 );
 INSERT INTO `nanyang_login`.`users`
-(`nric`, `fname`, `lname`, `gender`, `dob`, `email`, `password`, `role`, `specialization`, `url`, `attempt`)
+(`nric`, `fname`, `lname`, `gender`, `dob`, `email`, `password`, `role`, `specialization`, `url`, `attempt`, `lockout`, `lockout_time`)
 VALUES
-('T0392511G', 'Daniel', 'Jack', 'M', '1999-10-22', 'danieljack@gmail.com','password', 'Patient', NULL, NULL, 0),
-('T1111111F', 'Eric', 'Lee', 'M', '1994-10-30', 'samwilson@gmail.com', 'password', 'Doctor', 'Cardiology', 'https://google.com', 0),
-('T5739128U', 'Chloe', 'Soh', 'F', '1980-10-15', 'chloesoh@gmail.com', 'password', 'Admin', NULL, NULL, 0);
+('T0392511G', 'Daniel', 'Jack', 'M', '1999-10-22', 'danieljack@gmail.com','password', 'Patient', NULL, NULL, 0, 'false', NULL),
+('T1111111F', 'Eric', 'Lee', 'M', '1994-10-30', 'samwilson@gmail.com', 'password', 'Doctor', 'Cardiology', 'https://google.com', 0, 'false', NULL),
+('T5739128U', 'Chloe', 'Soh', 'F', '1980-10-15', 'chloesoh@gmail.com', 'password', 'Admin', NULL, NULL, 0, 'false', NULL);
