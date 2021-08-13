7778be292be9580773ed905ae5081e127e980e81
JabezThian <56944525+JabezThian@users.noreply.github.com>
2021-08-14 07:22:42 +0800

    Update passGenerate.py by Jabez

--- passGenerate.py
+++ passGenerate.py
@@ -4,3 +4,7 @@ import string, random
 def new_pass(size=8, chars = string.ascii_letters + string.digits):
     print('For Programmer: INFORMATION --> New Password has been created!')
     return ''.join(random.choice(chars) for _ in range(size))
+
+def new_code(size=6, chars = string.ascii_letters + string.digits):
+    print('For Programmer: INFORMATION --> New Password has been created!')
+    return ''.join(random.choice(chars) for _ in range(size))
