0aecbaeb8c8a87181d0cbf867779f5bb2d6e0f06
Isaackec <83005825+Isaackec@users.noreply.github.com>
2021-08-15 23:22:51 +0800

    Update Nanyang_Polyclinic.sql

--- Nanyang_Polyclinic.sql
+++ Nanyang_Polyclinic.sql
@@ -20,8 +20,8 @@ CREATE TABLE IF NOT EXISTS users (
  PRIMARY KEY (nric)
 );
 INSERT INTO `nanyang_login`.`users`
-(`nric`, `fname`, `lname`, `gender`, `dob`, `email`, `password`, `role`, `specialization`, `url`, `attempt`, `lockout`, `lockout_time`, `reset_password`)
+(`nric`, `fname`, `lname`, `gender`, `dob`, `email`, `password`, `symmetrickey`, `role`, `specialization`, `url`, `attempt`, `lockout`, `lockout_time`, `reset_password`)
 VALUES
-('T0392511G', 'Daniel', 'Jack', 'M', '1999-10-22', 'danieljack@gmail.com','password', 'Patient', NULL, NULL, 0, 'false', NULL, 'F'),
-('T1111111F', 'Eric', 'Lee', 'M', '1994-10-30', 'samwilson@gmail.com', 'password', 'Doctor', 'Cardiology', 'https://google.com', 0, 'false', NULL, 'F'),
-('T5739128U', 'Chloe', 'Soh', 'F', '1980-10-15', 'chloesoh@gmail.com', 'password', 'Admin', NULL, NULL, 0, 'false', NULL, 'F');
+('T0392511G', 'Daniel', 'Jack', 'M', '1999-10-22', 'danieljack@gmail.com','gAAAAABhGSCC3f-RLC1qjMTYk4RZOIuTHusv0vQNrBgVoUsgkKIgxrqmG7MnNsXcV1BHiTcaYTEbYbsJ-kOR7GjUMitYhQ73Tg==', 'J6HdU98z_PBxzw_15jOXiGMMuFEsmzlXcysQDnsx_IE=', 'Patient', NULL, NULL, 0, 'false', NULL, 'F'),
+('T1111111F', 'Eric', 'Lee', 'M', '1994-10-30', 'samwilson@gmail.com', 'gAAAAABhGSCC3f-RLC1qjMTYk4RZOIuTHusv0vQNrBgVoUsgkKIgxrqmG7MnNsXcV1BHiTcaYTEbYbsJ-kOR7GjUMitYhQ73Tg==', 'J6HdU98z_PBxzw_15jOXiGMMuFEsmzlXcysQDnsx_IE=', 'Doctor', 'Cardiology', 'https://google.com', 0, 'false', NULL, 'F'),
+('T5739128U', 'Chloe', 'Soh', 'F', '1980-10-15', 'chloesoh@gmail.com', 'gAAAAABhGSCC3f-RLC1qjMTYk4RZOIuTHusv0vQNrBgVoUsgkKIgxrqmG7MnNsXcV1BHiTcaYTEbYbsJ-kOR7GjUMitYhQ73Tg==', 'J6HdU98z_PBxzw_15jOXiGMMuFEsmzlXcysQDnsx_IE=','Admin', NULL, NULL, 0, 'false', NULL, 'F');
