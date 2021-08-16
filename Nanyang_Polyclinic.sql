b7815905e0034ba399cc9b1391b89982e0ea0a7e
JabezThian <56944525+JabezThian@users.noreply.github.com>
2021-08-16 21:18:10 +0800

    Update Nanyang_Polyclinic.sql

--- Nanyang_Polyclinic.sql
+++ Nanyang_Polyclinic.sql
@@ -24,4 +24,4 @@ INSERT INTO `nanyang_login`.`users`
 VALUES
 ('T0392511G', 'Daniel', 'Jack', 'M', '1999-10-22', 'nugget6chicken@gmail.com','gAAAAABhGSCC3f-RLC1qjMTYk4RZOIuTHusv0vQNrBgVoUsgkKIgxrqmG7MnNsXcV1BHiTcaYTEbYbsJ-kOR7GjUMitYhQ73Tg==', 'J6HdU98z_PBxzw_15jOXiGMMuFEsmzlXcysQDnsx_IE=', 'Patient', NULL, NULL, 0, 'false', NULL, 'F'),
 ('T1111111F', 'Eric', 'Lee', 'M', '1994-10-30', 'nanyanghospital2021@gmail.com', 'gAAAAABhGSCC3f-RLC1qjMTYk4RZOIuTHusv0vQNrBgVoUsgkKIgxrqmG7MnNsXcV1BHiTcaYTEbYbsJ-kOR7GjUMitYhQ73Tg==', 'J6HdU98z_PBxzw_15jOXiGMMuFEsmzlXcysQDnsx_IE=', 'Doctor', 'Cardiology', 'https://google.com', 0, 'false', NULL, 'F'),
-('T5739128U', 'Chloe', 'Soh', 'F', '1980-10-15', 'nuggetchicken2021@gmail.com', 'gAAAAABhGSCC3f-RLC1qjMTYk4RZOIuTHusv0vQNrBgVoUsgkKIgxrqmG7MnNsXcV1BHiTcaYTEbYbsJ-kOR7GjUMitYhQ73Tg==', 'J6HdU98z_PBxzw_15jOXiGMMuFEsmzlXcysQDnsx_IE=','Admin', NULL, NULL, 0, 'false', NULL, 'F');
+('T5739128U', 'Chloe', 'Soh', 'F', '1980-10-15', 'cholesoh@gmail.com', 'gAAAAABhGSCC3f-RLC1qjMTYk4RZOIuTHusv0vQNrBgVoUsgkKIgxrqmG7MnNsXcV1BHiTcaYTEbYbsJ-kOR7GjUMitYhQ73Tg==', 'J6HdU98z_PBxzw_15jOXiGMMuFEsmzlXcysQDnsx_IE=','Admin', NULL, NULL, 0, 'false', NULL, 'F');
